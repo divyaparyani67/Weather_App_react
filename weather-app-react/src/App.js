e17bb12e5c6578734b11763e3e02696792aa514e
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-02-20 22:35:14 +0530

    WEATHER API

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -3,7 +3,7 @@ import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { useState } from "react";
 import Forecast from "./components/forecast/forecast";
-import { Weather_API_URL, Weather_API_KEY } from "./api";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
