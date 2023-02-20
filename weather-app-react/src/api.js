a6e349d2501cc50ee93ad8e334e6abc7470c3842
divyaparyani67 <divyaparyani67@gmail.com>
2023-02-20 22:14:10 +0530

    added components

---
+++ weather-app-react/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e18eef537emshd92614120812e12p1b5e97jsne69c0f666fae",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
+export const Weather_API_KEY = "";
